600a73d14b07400109dfb1298adbde6ded2d1209
suryansh2206 <suryansh804.sg@gmail.com>
2023-04-12 17:35:20 +0530

    Accessing UserInputs

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredAge, enteredUsername);
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+          required
+        />
 
         <label htmlFor="age">Age(years)</label>
-        <input id="age" type="number" min="18" max="60" />
+        <input
+          id="age"
+          type="number"
+          min="18"
+          max="60"
+          onChange={ageChangeHandler}
+          required
+        />
 
         <Button type="submit">Add user</Button>
       </form>
